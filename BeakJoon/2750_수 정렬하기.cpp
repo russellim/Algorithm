657606173501788ecdab974d851dcb095ce78ae6
russellim <neneg273@gmail.com>
2020-08-25 19:53:09 +0900

    2750_수 정렬하기.cpp

---
+++ BeakJoon/2750_수 정렬하기.cpp
@@ -0,0 +1,142 @@
+﻿// 20.08.25 TUE
+// 2750: 수 정렬 https://www.acmicpc.net/problem/2750
+// 정렬
+
+// 퀵정렬, 힙정렬1, 힙정렬2.
+
+#include <iostream>
+using namespace std;
+
+void quick_sort(int* arr, int start, int end)
+{
+	// 평균속도 빠름.
+	// 정렬이 미리 된 배열은 안됨.
+	if (start >= end) return;
+	int pivot = start;
+	int i = start + 1, j = end;
+	int temp;
+
+	while (i <= j)
+	{
+		while (i <= end && arr[i] <= arr[pivot]) i++;
+		while (j >= 0 && arr[j] >= arr[pivot]) j--;
+		if (i > j)
+		{
+			temp = arr[pivot];
+			arr[pivot] = arr[j];
+			arr[j] = temp;
+		}
+		else
+		{
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+	quick_sort(arr, start, j - 1);
+	quick_sort(arr, j + 1, end);
+}
+
+void heap_sort(int* arr, int size)
+{
+	// 한번 힙 구조 만들고 자식 크기 비교하며 정렬.
+	// 최대 힙 구조로 만듦.
+	for (int i = 1; i < size; ++i)
+	{
+		int child = i;
+		do
+		{
+			int root = (child - 1) / 2;
+			if (arr[root] < arr[child])
+			{
+				int temp = arr[root];
+				arr[root] = arr[child];
+				arr[child] = temp;
+			}
+			child = root;
+		} while (child != 0);
+	}
+	// 젤앞에 있는 젤 큰 값을 뒤로 보냄.
+	for (int i = size - 1; i >= 0; --i)
+	{
+		int temp = arr[0];
+		arr[0] = arr[i];
+		arr[i] = temp;
+		int child;
+		int root = 0;
+		do
+		{
+			child = 2 * root + 1;
+			//자식 중에 더 큰 값 찾기.
+			if (arr[child] < arr[child + 1] && child < i - 1)
+			{
+				child++;
+			}
+			// 루트보다 자식이 더 크면 교환.
+			if (arr[root] < arr[child] && child < i)
+			{
+				int temp = arr[root];
+				arr[root] = arr[child];
+				arr[child] = temp;
+			}
+			root = child;
+		} while (child < i);
+	}
+
+}
+
+void heap_sort2(int* arr, int size)
+{
+	// 배열 탐색 크기 줄여가며 힙만들기 반복.
+	// 최대 힙 구조로 만듦.
+	for (int i = 1; i < size; ++i)
+	{
+		int child = i;
+		do
+		{
+			int root = (child - 1) / 2;
+			if (arr[root] < arr[child])
+			{
+				int temp = arr[root];
+				arr[root] = arr[child];
+				arr[child] = temp;
+			}
+			child = root;
+		} while (child != 0);
+	}
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL); cin.tie(NULL);
+
+	int a;
+	cin >> a;
+	int *arr = new int[a];
+	for (int i = 0; i < a; ++i)
+	{
+		cin >> arr[i];
+	}
+
+	// 방법 1: stl의 sort쓰는게 제일 간단.
+	// 방법 2: quick_sort(arr, 0, a);
+	// 방법 3: heap_sort(arr, a);
+	// 방법 4:
+	heap_sort2(arr, a);
+	for (int i = a-1; i >= 0; --i)
+	{
+		int temp = arr[i];
+		arr[i] = arr[0];
+		arr[0] = temp;
+
+		heap_sort2(arr, i);
+	}
+
+
+	for (int i = 0; i < a; ++i)
+	{
+		cout << arr[i] << "\n";
+	}
+}
