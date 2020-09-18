61bb5dbaa3704437f717e6215c61be11eaf0bf0b
russellim <neneg273@gmail.com>
2020-09-18 18:42:45 +0900

    2606_바이러스.cpp

---
+++ BeakJoon/2606_바이러스.cpp
@@ -0,0 +1,48 @@
+﻿// 20.09.18 금
+// 2606: 바이러스 https://www.acmicpc.net/problem/2606
+// DFS
+#include <iostream>
+using namespace std;
+
+#define MAX 101
+int n, m;
+int mat[MAX][MAX];
+bool visit[MAX];
+
+void dfs(int v)
+{
+	visit[v] = true;
+	for (int i = 1; i <= n; ++i)
+	{
+		// 방문했거나, 연결 ㄴㄴ -> 다음꺼 탐색.
+		if (visit[i] == true || mat[v][i] == 0)
+			continue;
+		dfs(i);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL); cin.tie(NULL);
+
+	int a, b;
+	cin >> n >> m;
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> a >> b;
+		mat[a][b] = mat[b][a] = 1;
+	}
+
+	dfs(1);
+
+	// 처음 시작인 1번은 빼니까 -1.
+	int sum = -1;
+	for (int i = 1; i <= n; ++i)
+	{
+		if (visit[i]) sum++;
+	}
+	cout << sum;
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -12,6 +12,7 @@ https://solved.ac/profile/neneg273
 2156: 포도주 시식 DP https://www.acmicpc.net/problem/2156  
 2178: 미로 탐색 BFS https://www.acmicpc.net/problem/2178  
 2579: 계단 오르기 DP https://www.acmicpc.net/problem/2579  
+2606: 바이러스 DFS https://www.acmicpc.net/problem/2606  
 2667: 단지번호붙이기 BFS https://www.acmicpc.net/problem/2667  
 2750: 수 정렬 HeapSort https://www.acmicpc.net/problem/2750  
 2751: 수 정렬하기 2 MergeSort https://www.acmicpc.net/problem/2751  
