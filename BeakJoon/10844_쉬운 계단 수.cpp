50efb571eee15e640d4f4e4ba1858a73d68e4441
russellim <neneg273@gmail.com>
2020-09-20 23:25:53 +0900

    10844_쉬운 계단 수.cpp

---
+++ BeakJoon/10844_쉬운 계단 수.cpp
@@ -0,0 +1,41 @@
+﻿qk// 20.09.20 일
+// 10844: 쉬운 계단 수 https://www.acmicpc.net/problem/10844
+// DP
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL); cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	long long arr[101][11] = { 0 };
+
+	for (int i = 0; i <= 9; ++i)
+	{
+		arr[1][i] = 1;
+	}
+
+
+	for (int i = 2; i <= n; ++i)
+	{
+		arr[i][0] = arr[i - 1][1];
+		for (int j = 1; j <= 9; ++j)
+		{
+			arr[i][j] = (arr[i - 1][j - 1] + arr[i - 1][j + 1]) % 1000000000;
+			//cout << arr[i][j] << endl;
+		}
+	}
+
+	long long sum = 0;
+	for (int i = 1; i < 10; ++i)
+	{
+		sum += arr[n][i];
+	}
+	cout << sum % 1000000000;
+
+
+	return 0;
+}
