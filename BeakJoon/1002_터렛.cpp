243afdeef765b82c73e15f89b3070f573e538639
russellim <neneg273@gmail.com>
2020-09-22 23:52:37 +0900

    1002_터렛.cpp

---
+++ BeakJoon/1002_터렛.cpp
@@ -0,0 +1,55 @@
+﻿// 20.09.22 화
+// 1002: 터렛 https://www.acmicpc.net/problem/1002
+// math
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t > 0)
+	{
+		t--;
+
+		int x1, y1, r1, x2, y2, r2, res;
+		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+
+		double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+		int minus = abs(r1 - r2);
+		int plus = r1 + r2;
+
+		if (distance == 0.0)
+		{
+			if (r1 == r2)
+			{
+				res = -1;
+			}
+			else
+			{
+				res = 0;
+			}
+
+		}
+		else
+		{
+			if (minus < distance && plus > distance)
+			{
+				res = 2;
+			}
+			else if (minus == distance || plus == distance)
+			{
+				res = 1;
+			}
+			else
+			{
+				res = 0;
+			}
+		}
+		cout << res << "\n";
+	}
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Study algorithm 2020.08.23 ~
 https://solved.ac/profile/neneg273  
 
 ## BOJ
+1002: 터렛 Math https://www.acmicpc.net/problem/1002  
 1012: 유기농 배추 BFS https://www.acmicpc.net/problem/1012
 1260: DFS와 BFS https://www.acmicpc.net/problem/1260  
 1463: 1로 만들기 DP https://www.acmicpc.net/problem/1463  
