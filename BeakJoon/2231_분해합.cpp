234d9c27d91859a2c1559c217a3413e5d3a64ba7
russellim <neneg273@gmail.com>
2020-09-27 21:50:26 +0900

    2231_분해합.cpp

---
+++ BeakJoon/2231_분해합.cpp
@@ -0,0 +1,39 @@
+﻿// 20.09.27 일
+// 2231: 분해합 https://www.acmicpc.net/problem/2231
+// Brute Force
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, sum;
+	cin >> n;
+
+	if (n == 1)
+	{
+		cout << "0";
+		return 0;
+	}
+
+	for (int i = 1; i <= n; ++i)
+	{
+		int temp = i;
+		sum = temp;
+		while (1)
+		{
+			sum += temp % 10;
+			temp /= 10;
+			if (temp == 0) break;
+		}
+		
+		if (sum == n)
+		{
+			cout << i;
+			break;
+		}
+
+		if (i == n) cout << "0";
+	}
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -15,6 +15,7 @@ https://solved.ac/profile/neneg273
 1932: 정수 삼각형 DP https://www.acmicpc.net/problem/1932  
 2156: 포도주 시식 DP https://www.acmicpc.net/problem/2156  
 2178: 미로 탐색 BFS https://www.acmicpc.net/problem/2178  
+2231: 분해합 BF https://www.acmicpc.net/problem/2231  
 2579: 계단 오르기 DP https://www.acmicpc.net/problem/2579  
 2606: 바이러스 DFS https://www.acmicpc.net/problem/2606  
 2667: 단지번호붙이기 BFS https://www.acmicpc.net/problem/2667  
