cd3430d80dfa374270e92746fc5b55387f343ddd
russellim <neneg273@gmail.com>
2020-09-30 19:45:32 +0900

    9655_돌 게임.cpp

---
+++ BeakJoon/9655_돌 게임.cpp
@@ -0,0 +1,22 @@
+﻿// 20.09.17 수
+// 9655: 숨바꼭질 https://www.acmicpc.net/problem/9655
+// DP
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if(n == 3)
+    {
+        cout << "SK";
+    }
+    else
+        {
+        if(n%2 == 0)
+            cout << "CY";
+        else
+            cout << "SK";
+        }
+}
