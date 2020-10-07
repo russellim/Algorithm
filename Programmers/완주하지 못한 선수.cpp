f5770df6a418197431533d5904386f4128d3d395
russellim <neneg273@gmail.com>
2020-10-07 23:13:47 +0900

    완주하지 못한 선수.cpp kakaoBlind->Programmers 폴더명 변경

---
+++ Programmers/완주하지 못한 선수.cpp
@@ -0,0 +1,38 @@
+﻿// 20.10.07. 수
+// 완주하지 못한 선수  https://programmers.co.kr/learn/courses/30/lessons/42576
+// Sort, 효율성 체크 있음, IDE 사용 금지!
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    int csize = completion.size();
+    answer = participant[participant.size() - 1];
+    for (int i = 0; i < csize; ++i)
+    {
+        if (participant[i] != completion[i])
+        {
+            answer = participant[i];
+            break;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL); cin.tie(NULL);
+
+    cout << solution({ "leo", "kiki", "eden" }, { "eden, kiki" }) << endl;
+    cout << solution({ "marina", "josipa", "nikola", "vinko", "filipa" }, { "josipa", "filipa", "marina", "nikola" }) << endl;
+    cout << solution({ "mislav", "stanko", "mislav", "ana" }, { "stanko", "ana", "mislav" }) << endl;
+
+    return 0;
+}
