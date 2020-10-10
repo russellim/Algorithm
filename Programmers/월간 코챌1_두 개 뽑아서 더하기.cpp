798fe64d6dc93ecacc60743f16834eade223ee0b
russellim <neneg273@gmail.com>
2020-10-10 21:18:21 +0900

    월간 코챌1_두 개 뽑아서 더하기.cpp

---
+++ Programmers/월간 코챌1_두 개 뽑아서 더하기.cpp
@@ -0,0 +1,49 @@
+﻿// 20.10.10. 토
+// 월간 코챌1: 두 개 뽑아서 더하기  https://programmers.co.kr/learn/courses/30/lessons/68644
+// Set
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    set<int> s;
+
+    for (int i = 0; i < numbers.size() - 1; ++i)
+    {
+        for (int j = i + 1; j < numbers.size(); ++j)
+        {
+            s.insert(numbers[i] + numbers[j]);
+        }
+    }
+
+    answer.assign(s.begin(), s.end());
+
+    return answer;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL); cin.tie(NULL);
+
+    vector<int> v;
+    v = solution({ 2,1,3,4,1 });
+    for (auto& x : v)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    v = solution({ 5,0,2,7 });
+    for (auto& x : v)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -45,6 +45,7 @@ https://solved.ac/profile/neneg273
 ## programmers
 #### Level 1
 완주하지 못한 선수 Hash https://programmers.co.kr/learn/courses/30/lessons/42576  
+월간 코챌1: 두 개 뽑아서 더하기  https://programmers.co.kr/learn/courses/30/lessons/68644  
 kakao 2018: 비밀지도 https://programmers.co.kr/learn/courses/30/lessons/17681  
 kakao 2019겨울인턴: 크레인 인형뽑기 게임 https://programmers.co.kr/learn/courses/30/lessons/64061  
 #### Level 2
