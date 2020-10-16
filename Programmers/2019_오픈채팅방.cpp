cae49b33b471a7dfca40ec488a617bb28b9a13c4
russellim <neneg273@gmail.com>
2020-10-17 00:57:53 +0900

    2019_오픈채팅방.cpp 수정 map의 key 활용

--- Programmers/2019_오픈채팅방.cpp
+++ Programmers/2019_오픈채팅방.cpp
@@ -1,72 +1,51 @@
 ﻿// 20.10.16. 금
 // 2019_오픈채팅방 https://programmers.co.kr/learn/courses/30/lessons/42888
 // IDE 사용 금지.
+
+// 수정 후, 굉장히 빨라졌음! map의 key 사용을 잘하자.
 #include <string>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 #include <sstream>
+#include <map>
 
 using namespace std;
 
 vector<string> solution(vector<string> record) {
     vector<string> answer;
-    vector<pair<string, string> > users;
-    vector<pair<string, string> > chatlogs;
-    string cmd, id, nick;
+    map<string, string> users;				// 유저 정보 (id, nickname).
+    vector<pair<string, string> > chatlogs;	// 채팅 로그(id, "님이 ~~.").
     for(int i=0; i<record.size(); ++i)
     {
-	cmd = record[i].substr(0, record[i].find(' '));
-        record[i] = record[i].substr(record[i].find(' ')+1);
-        id = record[i].substr(0, record[i].find(' '));
-        // users에 데이터가 있는지 검사
-        auto it = users.begin();
-        for(; it != users.end(); ++it)
+        string* cmd = new string[3];
+
+		// ' ' 기준으로 문자열 자르기 유용! (sstream).
+        istringstream ss(record[i]);
+        int j=0;
+        while (getline (ss, cmd[j], ' ')) ++j;
+        
+        if(cmd[0][0] == 'L')
         {
-            if(it->first == id)
-            {
-                break;
-            }
-        }
-        if(it == users.end())
-        {
-            // users에 데이터 없음. ""Enter"" 처음 한 칭구만 여기 걸림.
-            record[i] = record[i].substr(record[i].find(' ')+1);
-            nick = record[i];
-            users.push_back({id, nick});
-            chatlogs.push_back({id, "님이 들어왔습니다."});
+            chatlogs.push_back({cmd[1], "님이 나갔습니다."});
         }
         else
         {
-            // users에 데이터 있음. Enter, Leave, Change.
-            if(cmd == "Leave")
+             users[cmd[1]] = cmd[2]; // key값이 없으면 생성, 이미 있으면 덮어쓰기.
+									 // insert는 덮어쓰기가 안됨.
+            if(cmd[0][0] == 'E')
             {
-                chatlogs.push_back({id, "님이 나갔습니다."});
-            }
-            else // Enter, Change.
-            {
-                record[i] = record[i].substr(record[i].find(' ')+1);
-                nick = record[i];
-                if(cmd == "Enter")
-                {
-                    chatlogs.push_back({id, "님이 들어왔습니다."});
-                }
-                it->second = nick;
+                chatlogs.push_back({cmd[1], "님이 들어왔습니다."});
             }
         }
+        delete[] cmd;
     }
     
     for(int i=0; i<chatlogs.size(); ++i)
     {
-        auto it = users.begin();
-        for(; it != users.end(); ++it)
-        {
-            if(it->first == chatlogs[i].first)
-            {
-                break;
-            }
-        }
-        answer.push_back(it->second + chatlogs[i].second);
+		// 채팅 로그의 아이디 -> 닉네임 변환.
+		// map[key]로 value 가져올 수 있음!.
+        answer.push_back(users[chatlogs[i].first] + chatlogs[i].second);
     }
     
     return answer;
