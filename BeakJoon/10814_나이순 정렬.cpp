b2234cfbcd68f7b9b0aedae3d59c40d3d7b794e3
russellim <neneg273@gmail.com>
2020-11-15 23:36:21 +0900

    10814_나이순 정렬.cpp

---
+++ BeakJoon/10814_나이순 정렬.cpp
@@ -0,0 +1,39 @@
+﻿// 20.11.15. 일
+// 10814: 나이순 정렬 https://www.acmicpc.net/problem/10814
+// stable_sort
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Member
+{
+	int age;
+	string name;
+	Member(int age, string name) : name(name), age(age) {}
+	bool operator<(const Member& m) const { return age < m.age; } // 정렬시 나이만 고려함.
+};
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cout.tie(0); cin.tie(0);
+
+	vector<Member> members;
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		int age; string name;
+		cin >> age >> name;
+		members.push_back(Member(age, name));
+	}
+	// 나이가 같으면 순서가 유지됩니다.
+	stable_sort(members.begin(), members.end());
+
+	for (auto& x : members)
+		cout << x.age << " " << x.name << "\n";
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -33,6 +33,7 @@ https://solved.ac/profile/neneg273
 7568: 덩치 BF https://www.acmicpc.net/problem/7568  
 9465: 스티커 DP https://www.acmicpc.net/problem/9465  
 9655: 돌 게임 https://www.acmicpc.net/problem/9655  
+10814: 나이순 정렬 StableSort https://www.acmicpc.net/problem/10814  
 10815: 숫자 카드 Sort https://www.acmicpc.net/problem/10815  
 10816: 숫자 카드 2 Search https://www.acmicpc.net/problem/10816  
 10844: 쉬운 계단 수 DP https://www.acmicpc.net/problem/10844  
