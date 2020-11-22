d9ff455c6770aa81bcb13f84d7ce7662f9d0ebec
russellim <neneg273@gmail.com>
2020-11-23 02:50:59 +0900

    2490_윷놀이.cpp 누락수정

---
+++ BeakJoon/2490_윷놀이.cpp
@@ -0,0 +1,33 @@
+﻿// 20.11.22. 일
+// 2490: 윷놀이 https://www.acmicpc.net/problem/2490
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    for (int i = 0; i < 3; ++i)
+    {
+        int one = 0;
+        for (int j = 0; j < 4; ++j)
+        {
+            int temp;
+            cin >> temp;
+            if (temp == 1) one++;
+        }
+        char ch;
+        switch (one)
+        {
+        case 0: ch = 'D'; break;
+        case 1: ch = 'C'; break;
+        case 2: ch = 'B'; break;
+        case 3: ch = 'A'; break;
+        case 4: ch = 'E'; break;
+        }
+        cout << ch << "\n";
+
+    }
+    return 0;
+}
