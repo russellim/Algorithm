d4f2b29ae7c5e2b8fc63559618e8c4cd61c78142
russellim <neneg273@gmail.com>
2020-11-27 20:41:33 +0900

    15686_치킨 배달.cpp 수정

--- BeakJoon/15686_치킨 배달.cpp
+++ BeakJoon/15686_치킨 배달.cpp
@@ -6,67 +6,71 @@
 #include <algorithm>
 using namespace std;
 
-typedef pair<int, int> P;
+typedef pair<int, int> P; // 좌표.
+int n, m; // n x n / m개 치킨집.
+int answer = 1e9; // 치킨거리 최솟값.
+vector<P> homes;
+vector<P> chickens;
+vector<vector<int> > dists;
+vector<int> indexes;
+
+void GetChickenDistance(int count, int start)
+{
+	if (count == m)
+	{
+		int sumDist = 0;
+		vector<int> disttemp;
+
+		for (int i = 0; i < dists.size(); ++i)
+		{
+			disttemp.clear();
+			for (int j = 0; j < indexes.size(); ++j)
+			{
+				disttemp.push_back(dists[i][indexes[j]]);
+			}
+			sumDist += *min_element(disttemp.begin(), disttemp.end());
+		}
+		answer = min(answer, sumDist);
+
+		return;
+	}
+	for (int i = start; i < chickens.size(); ++i)
+	{
+		indexes.push_back(i);
+		GetChickenDistance(count + 1, i + 1);
+		indexes.pop_back();
+	}
+}
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cout.tie(0); cin.tie(0);
 
-
-	int n, m, answer = 0;
 	cin >> n >> m;
-	vector<vector<int> > city(n, vector<int>(n));
-	vector<P> chickens, homes;
+
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < n; ++j)
 		{
-			cin >> city[i][j];
-			if (city[i][j] == 2) chickens.push_back({ i,j });
-			else if (city[i][j] == 1) homes.push_back({ i, j });
-		}
-	}
-	vector<vector<int> > dist(chickens.size());
-	for (int i = 0; i < chickens.size(); ++i)
-	{
-		dist[i].push_back(0);
-		for (int j = 0; j < homes.size(); ++j)
-		{
-			int temp = abs(chickens[i].first - homes[j].first) + abs(chickens[i].second - homes[j].second);
-			dist[i][0] += temp;
-			dist[i].push_back(temp);
+			int temp;
+			cin >> temp;
+			if (temp == 1) homes.push_back({ i,j });
+			else if (temp == 2) chickens.push_back({ i,j });
 		}
 	}
 
-	sort(dist.begin(), dist.end());
+	dists.assign(homes.size(), vector<int>(chickens.size()));
 
-	for (int i = 0; i < chickens.size(); ++i)
+	for (int i = 0; i < homes.size(); ++i)
 	{
-		for (int j = 0; j < homes.size() + 1; ++j)
+		for (int j = 0; j < chickens.size(); ++j)
 		{
-			cout << dist[i][j] << " ";
+			dists[i][j] = abs(homes[i].first - chickens[j].first) + abs(homes[i].second - chickens[j].second);
 		}
-		cout << endl;
 	}
-
-	if (m == 1)
-	{
-		cout << dist[0][0];
-	}
-	else
-	{
-		for (int j = 1; j < homes.size() + 1; ++j)
-		{
-			vector<int> temp;
-			for (int i = 0; i < m; ++i)
-			{
-				temp.push_back(dist[i][j]);
-			}
-			answer += *min_element(temp.begin(), temp.end());
-		}
-		cout << answer;
-	}
+	GetChickenDistance(0, 0);
+	cout << answer;
 
 	return 0;
 }
