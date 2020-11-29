2be9a80066b364d42bce8989735f1ecd34c34261
russellim <neneg273@gmail.com>
2020-11-29 23:06:36 +0900

    2485_가로수.cpp 수정

--- BeakJoon/2485_가로수.cpp
+++ BeakJoon/2485_가로수.cpp
@@ -1,13 +1,12 @@
 ﻿// 20.11.28. 토
 // 2485: 가로수 https://www.acmicpc.net/problem/2485
 // Math, 유클리드 호제법
-
-// 6%
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
+// 최대공약수 구하기.
 int gcd(int a, int b)
 {
 	while (b != 0)
@@ -35,6 +34,7 @@ int main()
 		start = min(start, trees[i]);
 	}
 
+	// 모든 가로수의 첫번째 가로수와의 거리 구하기.
 	for (int i = 0; i < n; ++i)
 	{
 		if (start != trees[i])
@@ -42,8 +42,14 @@ int main()
 	}
 	sort(dists.begin(), dists.end());
 
-	int g = gcd(dists[1], dists[dists.size() - 1]);
+	// 거리들의 최대공약수 구하기.
+	int g = gcd(dists[1], dists[2]);
+	for (int i = 3; i < dists.size(); ++i)
+	{
+		g = gcd(g, dists[i]);
+	}
 
+	// 있어야하는 가로수 - 이미 있는 가로수.
 	cout << (dists[dists.size() - 1] / g) + 1 - dists.size();
 
 	return 0;
