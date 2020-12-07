ba7c184b199011982ccc6829befdbe679fd1de9b
russellim <neneg273@gmail.com>
2020-12-07 21:36:46 +0900

    11403_경로 찾기.cpp

---
+++ BeakJoon/11403_경로 찾기.cpp
@@ -0,0 +1,57 @@
+﻿// 20.12.07. 월
+// 11403: 경로 찾기 https://www.acmicpc.net/problem/11403
+// BFS.
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+	vector<vector<int> > in(n, vector<int>(n, 0));
+	auto answer = in;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cin >> in[i][j];
+		}
+	}
+
+	queue<int> q;
+	for (int i = 0; i < n; ++i)
+	{
+		q.push(i);
+		int pivot = i;
+		while (!q.empty())
+		{
+			int cur = q.front();
+			q.pop();
+			for (int j = 0; j < n; ++j)
+			{
+				if (in[cur][j] == 1 && answer[pivot][j] == 0)
+				{
+					q.push(j);
+					answer[pivot][j] = 1;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cout << answer[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -55,6 +55,7 @@ https://solved.ac/profile/neneg273
 11052: 카드 구매하기 DP https://www.acmicpc.net/problem/11052  
 11053: 가장 긴 증가하는 부분 수열 DP https://www.acmicpc.net/problem/11053  
 11057: 오르막 수 DP https://www.acmicpc.net/problem/11057  
+11403: 경로 찾기 BFS https://www.acmicpc.net/problem/11403  
 11659: 구간 합 구하기 4 부분합 https://www.acmicpc.net/problem/11659  
 11724: 연결 요소의 개수 BFS https://www.acmicpc.net/problem/11724  
 11728: 배열 합치기 MergeSort https://www.acmicpc.net/problem/11728  
