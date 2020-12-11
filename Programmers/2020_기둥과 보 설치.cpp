40f18a40b787e15a4591c7600b6a81e98c3e5697
russellim <neneg273@gmail.com>
2020-12-11 20:42:06 +0900

    2020_기둥과 보 설치.cpp 수정

--- Programmers/2020_기둥과 보 설치.cpp
+++ Programmers/2020_기둥과 보 설치.cpp
@@ -1,7 +1,6 @@
 ﻿// 20.12.10. 목
 // kakao 2020_기둥과 보 설치 https://programmers.co.kr/learn/courses/30/lessons/60061
-
-// fail.
+// 구현.
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,8 +8,25 @@
 
 using namespace std;
 
+// 기둥 생성 조건.
+bool IsCreatePillar(vector<vector<int> >& P, vector<vector<int> >& B, int x, int y)
+{
+    if (y == 0 || (x > 0 && B[x - 1][y] == 1) || B[x][y] == 1 || P[x][y - 1] == 1)
+        return true;
+    return false;
+}
+
+// 보 생성 조건.
+bool IsCreateBeam(vector<vector<int> >& P, vector<vector<int> >& B, int x, int y)
+{
+    if (P[x][y - 1] == 1 || (x < P.size() - 1 && P[x + 1][y - 1] == 1) || (x > 0 && B[x - 1][y] == 1 && x < B.size() - 1 && B[x + 1][y] == 1))
+        return true;
+    return false;
+}
+
 vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
-    vector<vector<int> > create(n + 1, vector<int>(n + 1, -1));
+    vector<vector<int> > Pillars(n + 1, vector<int>(n + 1, 0));
+    vector<vector<int> > Beams(n + 1, vector<int>(n + 1, 0));
     vector<vector<int> > answer;
     for (int i = 0; i < build_frame.size(); ++i)
     {
@@ -21,27 +37,34 @@ vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
         {
             if (build_frame[i][2] == 0) // 기둥.
             {
-                if (y > 0 && !((y > 0 && create[x][y - 1] == 0) || (x > 0 && create[x - 1][y] == 1) || create[x][y] == 1))
-                    continue;
-                create[x][y] = 0;
+                if (!IsCreatePillar(Pillars, Beams, x, y)) continue;
+                Pillars[x][y] = 1;
             }
             else    // 보.
             {
-                create[x][y] = 1;
+                if (!IsCreateBeam(Pillars, Beams, x, y)) continue;
+                Beams[x][y] = 1;
             }
         }
-        // 삭제.
+        // 삭제 (일단 삭제 해놓고 주변 기둥, 보가 생성조건에 위배되는가? -> true면 삭제 무시).
         else
         {
             if (build_frame[i][2] == 0) // 기둥.
             {
-                create[x][y] = -1;
+                Pillars[x][y] = 0;
+                if ((y < n - 1 && Pillars[x][y + 1] == 1 && !IsCreatePillar(Pillars, Beams, x, y + 1)) ||
+                    (y < n - 1 && Beams[x][y + 1] == 1 && !IsCreateBeam(Pillars, Beams, x, y + 1)) ||
+                    (y < n - 1 && x  > 0 && Beams[x - 1][y + 1] == 1 && !IsCreateBeam(Pillars, Beams, x - 1, y + 1)))
+                    Pillars[x][y] = 1;
             }
             else
             {
-                if (!((x > 0 && y > 0 && create[x - 1][y - 1] == 0) && (y > 0 && create[x + 1][y - 1] == 0)))
-                    continue;
-                create[x][y] = -1;
+                Beams[x][y] = 0;
+                if ((x < n - 1 && Pillars[x + 1][y] == 1 && !IsCreatePillar(Pillars, Beams, x + 1, y)) ||
+                    (Pillars[x][y] == 1 && !IsCreatePillar(Pillars, Beams, x, y)) ||
+                    (x > 0 && Beams[x - 1][y] == 1 && !IsCreateBeam(Pillars, Beams, x - 1, y)) ||
+                    (x < n - 1 && Beams[x + 1][y] == 1 && !IsCreateBeam(Pillars, Beams, x + 1, y)))
+                    Beams[x][y] = 1;
             }
         }
     }
@@ -50,12 +73,17 @@ vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
     {
         for (int j = 0; j < n + 1; ++j)
         {
-            if (create[i][j] != -1)
+            if (Pillars[i][j] == 1)
             {
-                answer.push_back({ i,j, create[i][j] });
+                answer.push_back({ i,j, 0 });
+            }
+            if (Beams[i][j] == 1)
+            {
+                answer.push_back({ i,j, 1 });
             }
         }
     }
+    sort(answer.begin(), answer.end());
 
     return answer;
 }
@@ -66,7 +94,29 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    solution(5, { {0, 0, 0, 1}, {2, 0, 0, 1}, {4, 0, 0, 1}, {0, 1, 1, 1}, {1, 1, 1, 1}, {2, 1, 1, 1}, {3, 1, 1, 1}, {2, 0, 0, 0}, {1, 1, 1, 0}, {2, 2, 0, 1} });
+    vector<vector<int>> v;
+    v = solution(5, { {1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1} });
+    for (int i = 0; i < v.size(); ++i)
+    {
+        for (int j = 0; j < v[i].size(); ++j)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+
+    v = solution(5, { {0, 0, 0, 1}, {2, 0, 0, 1}, {4, 0, 0, 1}, {0, 1, 1, 1}, {1, 1, 1, 1}, {2, 1, 1, 1}, {3, 1, 1, 1}, {2, 0, 0, 0}, {1, 1, 1, 0}, {2, 2, 0, 1} });
+    for (int i = 0; i < v.size(); ++i)
+    {
+        for (int j = 0; j < v[i].size(); ++j)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 
     return 0;
 }
