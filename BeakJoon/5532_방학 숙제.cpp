b26cbb6a2150b90a4ea2f40477f03170b90172dd
russellim <neneg273@gmail.com>
2020-12-24 18:28:07 +0900

    5532_방학 숙제.cpp

---
+++ BeakJoon/5532_방학 숙제.cpp
@@ -0,0 +1,23 @@
+﻿// 20.12.24. 목
+// 5532: 방학 숙제 https://www.acmicpc.net/problem/5532
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int L, B, A, C, D, b, a;
+    cin >> L >> B >> A >> C >> D;
+
+    b = B / C;
+    if (B % C != 0) b++;
+    a = A / D;
+    if (A % D != 0) a++;
+
+    cout << L - max(a, b);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -43,6 +43,7 @@ https://solved.ac/profile/neneg273
 4948: 베르트랑 공준 https://www.acmicpc.net/problem/4948  
 4963: 섬의 개수 BFS https://www.acmicpc.net/problem/4963  
 5430: AC Deque https://www.acmicpc.net/problem/5430  
+5532: 방학 숙제 https://www.acmicpc.net/problem/5532  
 6603: 로또 BT https://www.acmicpc.net/problem/6603  
 7576: 토마토 BFS https://www.acmicpc.net/problem/7576  
 7568: 덩치 BF https://www.acmicpc.net/problem/7568  
