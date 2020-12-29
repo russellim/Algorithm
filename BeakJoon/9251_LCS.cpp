0f2c7ddbdd2021ffe3d1615ebe6dddfd25b739cd
russellim <neneg273@gmail.com>
2020-12-29 20:39:23 +0900

    9251_LCS.cpp

---
+++ BeakJoon/9251_LCS.cpp
@@ -0,0 +1,39 @@
+﻿// 20.12.29. 화
+// 9251: LCS https://www.acmicpc.net/problem/9251
+// DP, Longest Common Subsequence (최장 공통 부분 수열).
+
+// LCS: 두 수열이 주어졌을 때, 부분 수열이 되는 수열 중 가장 긴 것을 찾는다.
+// ex> ACAYKP와 CAPCAK의 LCS는 ACAK
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string a, b;
+    cin >> a >> b;
+
+    // 각각 문자열 길이+1의 2차원 배열 준비.
+    vector<vector<int> > dp(b.size() + 1, vector<int>(a.size() + 1, 0));
+
+    for (int i = 1; i <= b.size(); ++i)
+    {
+        for (int j = 1; j <= a.size(); ++j)
+        {
+            // 비교 문자가 같으면, 왼쪽 대각선 위 값 +1
+            if (b[i - 1] == a[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            // 비교 문자가 다르면, max(왼쪽 값, 위쪽 값).
+            else
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+        }
+    }
+
+    cout << dp[b.size()][a.size()];
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -50,6 +50,7 @@ https://solved.ac/profile/neneg273
 7576: 토마토 BFS https://www.acmicpc.net/problem/7576  
 7568: 덩치 BF https://www.acmicpc.net/problem/7568  
 9020: 골드바흐의 추측 https://www.acmicpc.net/problem/9020  
+9251: LCS DP https://www.acmicpc.net/problem/9251  
 9465: 스티커 DP https://www.acmicpc.net/problem/9465  
 9655: 돌 게임 https://www.acmicpc.net/problem/9655  
 9663: N-Queen BT https://www.acmicpc.net/problem/9663  
