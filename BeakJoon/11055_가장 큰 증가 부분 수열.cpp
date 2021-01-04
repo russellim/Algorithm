1f9fcab73cd7581f1f3e2e04e853835f57199cdb
russellim <neneg273@gmail.com>
2021-01-04 20:36:50 +0900

    11055_가장 큰 증가 부분 수열.cpp

---
+++ BeakJoon/11055_가장 큰 증가 부분 수열.cpp
@@ -0,0 +1,36 @@
+﻿// 21.01.04. 월
+// 11055: 가장 큰 증가 부분 수열 https://www.acmicpc.net/problem/11055
+// DP.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, answer = 0;
+    cin >> n;
+    vector<int> v(n), dp(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> v[i];
+        dp[i] = v[i];
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            if (v[i] > v[j])
+            {
+                dp[i] = max(dp[j] + v[i], dp[i]);
+            }
+        }
+        answer = max(answer, dp[i]);
+    }
+    cout << answer;
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -70,6 +70,7 @@ https://solved.ac/profile/neneg273
 11051: 이항 계수 2 DP https://www.acmicpc.net/problem/11051  
 11052: 카드 구매하기 DP https://www.acmicpc.net/problem/11052  
 11053: 가장 긴 증가하는 부분 수열 DP https://www.acmicpc.net/problem/11053  
+11055: 가장 큰 증가 부분 수열 DP https://www.acmicpc.net/problem/11055  
 11057: 오르막 수 DP https://www.acmicpc.net/problem/11057  
 11403: 경로 찾기 BFS https://www.acmicpc.net/problem/11403  
 11659: 구간 합 구하기 4 부분합 https://www.acmicpc.net/problem/11659  
