73229d4421ce02c04d271d0a0cd53c1713428913
russellim <neneg273@gmail.com>
2021-01-08 23:44:57 +0900

    2217_로프.cpp

---
+++ BeakJoon/2217_로프.cpp
@@ -0,0 +1,29 @@
+﻿// 21.01.08. 금
+// 2217: 로프 https://www.acmicpc.net/problem/2217
+// 그리디.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, answer = 0;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n; ++i)
+        answer = max(answer, v[i] * (n - i));
+
+    cout << answer;
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -33,6 +33,7 @@ https://solved.ac/profile/neneg273
 1991: 트리 순회 Tree https://www.acmicpc.net/problem/1991  
 2156: 포도주 시식 DP https://www.acmicpc.net/problem/2156  
 2178: 미로 탐색 BFS https://www.acmicpc.net/problem/2178  
+2217: 로프 https://www.acmicpc.net/problem/2217  
 2231: 분해합 BF https://www.acmicpc.net/problem/2231  
 2293: 동전 1 DP https://www.acmicpc.net/problem/2293  
 2468: 안전 영역 BFS https://www.acmicpc.net/problem/2468  
