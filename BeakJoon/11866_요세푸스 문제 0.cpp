f13aaf661a082a0314ea280057951a6580db7599
russellim <neneg273@gmail.com>
2021-01-12 22:59:24 +0900

    11866_요세푸스 문제 0.cpp

---
+++ BeakJoon/11866_요세푸스 문제 0.cpp
@@ -0,0 +1,35 @@
+﻿// 21.01.12. 화
+// 11866: 요세푸스 문제 0 https://www.acmicpc.net/problem/11866
+// 큐.
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+    queue<int> q;
+    for (int i = 0; i < n; ++i)
+    {
+        q.push(i + 1);
+    }
+
+    cout << "<";
+    while (q.size() != 1)
+    {
+        for (int i = 0; i < k - 1; ++i)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front() << ", ";
+        q.pop();
+    }
+    cout << q.front() << ">";
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -80,6 +80,7 @@ https://solved.ac/profile/neneg273
 11659: 구간 합 구하기 4 부분합 https://www.acmicpc.net/problem/11659  
 11724: 연결 요소의 개수 BFS https://www.acmicpc.net/problem/11724  
 11728: 배열 합치기 MergeSort https://www.acmicpc.net/problem/11728  
+11866: 요세푸스 문제 0 https://www.acmicpc.net/problem/11866  
 11931: 수 정렬하기 4 Sort https://www.acmicpc.net/problem/11931  
 13458: 시험 감독 Math https://www.acmicpc.net/problem/13458  
 14500: 테트로미노 https://www.acmicpc.net/problem/14500  
