ae5b3de6630d74193393391698ae21c618fbee62
russellim <neneg273@gmail.com>
2021-01-25 18:51:49 +0900

    1927_최소 힙.cpp

---
+++ BeakJoon/1927_최소 힙.cpp
@@ -0,0 +1,36 @@
+﻿// 21.01.25. 월
+// 1927: 최소 힙 https://www.acmicpc.net/problem/1927
+// 우선순위 큐.
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, cmd;
+    cin >> n;
+    priority_queue<int> q;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> cmd;
+        if (cmd == 0)
+        {
+            if (q.empty())
+            {
+                cout << "0\n";
+                continue;
+            }
+            cout << -q.top() << "\n";
+            q.pop();
+            continue;
+        }
+        q.push(-cmd);
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -27,6 +27,7 @@ https://solved.ac/profile/neneg273
 1874: 스택 수열 Stack https://www.acmicpc.net/problem/1874  
 1912: 연속합 DP https://www.acmicpc.net/problem/1912  
 1920: 수 찾기 BinarySearch https://www.acmicpc.net/problem/1920  
+1927: 최소 힙 https://www.acmicpc.net/problem/1927  
 1931: 회의실배정 Sort https://www.acmicpc.net/problem/1931  
 1932: 정수 삼각형 DP https://www.acmicpc.net/problem/1932  
 1966: 프린터 큐 Queue https://www.acmicpc.net/problem/1966  
