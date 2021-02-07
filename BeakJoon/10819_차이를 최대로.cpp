ce1d4fc523ffa8de74d72a0d7bbd730a8fd5bad0
russellim <neneg273@gmail.com>
2021-02-07 15:32:20 +0900

    10819_차이를 최대로.cpp

---
+++ BeakJoon/10819_차이를 최대로.cpp
@@ -0,0 +1,68 @@
+﻿// 21.02.07. 일
+// 10819: 차이를 최대로 https://www.acmicpc.net/problem/10819
+// 백트래킹, 브루트포스.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+namespace Solution
+{
+#define DONTUSE -200
+    int n, answer = 0;
+    vector<int> arr, brute;
+
+    int Calcul()
+    {
+        int result = 0;
+        for (int i = 1; i < n; ++i)
+        {
+            result += abs(brute[i - 1] - brute[i]);
+        }
+        return result;
+    }
+
+    void BT(int count)
+    {
+        if (count == arr.size())
+        {
+            answer = max(answer, Calcul());
+        }
+
+        for (int i = 0; i < arr.size(); ++i)
+        {
+            if (arr[i] == DONTUSE) continue;
+            brute.push_back(arr[i]);
+            arr[i] = DONTUSE;
+            BT(count + 1);
+            arr[i] = brute.back();
+            brute.pop_back();
+        }
+    }
+
+    void Output()
+    {
+        cout << answer;
+    }
+    void Input()
+    {
+        cin >> n;
+        arr.assign(n, 0);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> arr[i];
+        }
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    Solution::Input();
+    Solution::BT(0);
+    Solution::Output();
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -80,6 +80,7 @@ https://solved.ac/profile/neneg273
 10814: 나이순 정렬 StableSort https://www.acmicpc.net/problem/10814  
 10815: 숫자 카드 Sort https://www.acmicpc.net/problem/10815  
 10816: 숫자 카드 2 Search https://www.acmicpc.net/problem/10816  
+10819: 차이를 최대로 BT https://www.acmicpc.net/problem/10819  
 10844: 쉬운 계단 수 DP https://www.acmicpc.net/problem/10844  
 10867: 중복 빼고 정렬하기 Vector https://www.acmicpc.net/problem/10867  
 10953: A+B - 6 String https://www.acmicpc.net/problem/10953  
