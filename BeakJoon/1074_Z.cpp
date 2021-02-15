369ea7c15c72ec2164d339674c4875b21f6fb3ce
russellim <neneg273@gmail.com>
2021-02-15 15:09:06 +0900

    1074_Z.cpp

---
+++ BeakJoon/1074_Z.cpp
@@ -0,0 +1,76 @@
+﻿// 21.02.15. 월
+// 1074: Z https://www.acmicpc.net/problem/1074
+// 분할정복, 재귀.
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+namespace Z
+{
+	int powN, r, c, num = -1;
+	bool IsFind = false;
+
+	void WriteZ(int a, int b)
+	{
+		for (int i = a; i < a + 2; ++i)
+		{
+			for (int j = b; j < b + 2; ++j)
+			{
+				num++;
+				if (i == r && j == c)
+				{
+					IsFind = true;
+					return;
+				}
+			}
+		}
+	}
+
+	void Divide(int n, int a, int b)
+	{
+		// 검사할 범위에 없으면 통째로 더하고 스킵.
+		if (!(r >= a && r < a + n && c >= b && c < b + n))
+		{
+			num += pow(n, 2);
+			return;
+		}
+
+		if (n == 2)
+		{
+			WriteZ(a, b);
+			return;
+		}
+
+		int half = n / 2;
+		for (int i = a; i <= a + half; i += half)
+		{
+			for (int j = b; j <= b + half; j += half)
+			{
+				Divide(half, i, j);
+				if (IsFind) return;
+			}
+		}
+	}
+
+	void Input()
+	{
+		int N;
+		cin >> N >> r >> c;
+		powN = pow(2, N);
+	}
+	void Output()
+	{
+		cout << num;
+	}
+}
+
+int main()
+{
+	Z::Input();
+	Z::Divide(Z::powN, 0, 0);
+	Z::Output();
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -9,6 +9,7 @@ https://solved.ac/profile/neneg273
 1012: 유기농 배추 BFS https://www.acmicpc.net/problem/1012  
 1018: 체스판 다시 칠하기 BF https://www.acmicpc.net/problem/1018  
 1021: 회전하는 큐 https://www.acmicpc.net/problem/1021  
+1074: Z 분할정복 https://www.acmicpc.net/problem/1074  
 1094: 막대기 https://www.acmicpc.net/problem/1094  
 1158: 요세푸스 문제 https://www.acmicpc.net/problem/1158  
 1181: 단어 정렬 String, Sort https://www.acmicpc.net/problem/1181  
