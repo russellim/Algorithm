913481a34e2bad1024a01d3b96747de8e8ee9a07
russellim <neneg273@gmail.com>
2021-02-16 20:53:27 +0900

    2630_색종이 만들기.cpp

---
+++ BeakJoon/2630_색종이 만들기.cpp
@@ -0,0 +1,75 @@
+﻿// 21.02.16. 화
+// 2630: 색종이 만들기 https://www.acmicpc.net/problem/2630
+// 분할정복
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace BOJ_2630
+{
+	int n;
+	vector<vector<int> > paper;
+	int color[2];
+
+	void Divide(int size, int starti, int startj);
+
+	void CheckColor(int size, int starti, int startj)
+	{
+		int FirstColor = paper[starti][startj];
+		for (int i = starti; i < starti + size; ++i)
+		{
+			for (int j = startj; j < startj + size; ++j)
+			{
+				if (FirstColor != paper[i][j])
+				{
+					Divide(size, starti, startj);
+					return;
+				}
+			}
+		}
+		color[FirstColor]++;
+	}
+
+	void Divide(int size, int starti, int startj)
+	{
+		int half = size / 2;
+		CheckColor(half, starti, startj);
+		CheckColor(half, starti, startj + half);
+		CheckColor(half, starti + half, startj);
+		CheckColor(half, starti + half, startj + half);
+	}
+
+	void Input()
+	{
+		cin >> n;
+		paper.assign(n, vector<int>(n));
+		bool IsAllSameColor = true;
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = 0; j < n; ++j)
+			{
+				cin >> paper[i][j];
+				if (paper[0][0] != paper[i][j]) IsAllSameColor = false;
+			}
+		}
+		if (!IsAllSameColor) Divide(n, 0, 0);
+		else color[paper[0][0]]++;
+	}
+
+	void Output()
+	{
+		cout << color[0] << "\n" << color[1];
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	BOJ_2630::Input();
+	BOJ_2630::Output();
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -59,6 +59,7 @@ https://solved.ac/profile/neneg273
 2580: 스도쿠 https://www.acmicpc.net/problem/2580  
 2583: 영역 구하기 BFS https://www.acmicpc.net/problem/2583  
 2606: 바이러스 DFS https://www.acmicpc.net/problem/2606  
+2630: 색종이 만들기 분할정복 https://www.acmicpc.net/problem/2630  
 2667: 단지번호붙이기 BFS https://www.acmicpc.net/problem/2667  
 2750: 수 정렬하기 HeapSort https://www.acmicpc.net/problem/2750  
 2751: 수 정렬하기 2 MergeSort https://www.acmicpc.net/problem/2751  
