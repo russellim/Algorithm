12e897b321079f13a14f9d3b6466296d80d5f631
russellim <neneg273@gmail.com>
2021-02-27 14:57:22 +0900

    15666_N과 M (12).cpp

---
+++ BeakJoon/15666_N과 M (12).cpp
@@ -0,0 +1,54 @@
+﻿// 21.02.27. 토
+// 15666: N과 M (12) https://www.acmicpc.net/problem/15666
+// 백트래킹.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+namespace BOJ15666
+{
+	int n, m;
+	vector<int> v, nums;
+	vector<vector<int> > answer;
+
+	void MakeNums(int count)
+	{
+		if (count == m)
+		{
+			answer.push_back(nums);
+			return;
+		}
+		for (int i = 0; i < n; ++i)
+		{
+			if (!nums.empty() && nums.back() > v[i]) continue;
+			nums.push_back(v[i]);
+			MakeNums(count + 1);
+			nums.pop_back();
+		}
+	}
+
+	void Solution()
+	{
+		cin >> n >> m;
+		v.assign(n, 0);
+		for (int i = 0; i < n; ++i)
+			cin >> v[i];
+		sort(v.begin(), v.end());
+		MakeNums(0);
+
+		sort(answer.begin(), answer.end());
+		answer.erase(unique(answer.begin(), answer.end()), answer.end());
+		for (int i = 0; i < answer.size(); ++i)
+		{
+			for (int x : answer[i]) cout << x << " ";
+			cout << "\n";
+		}
+	}
+}
+
+int main()
+{
+	BOJ15666::Solution();
+}
--- README.md
+++ README.md
@@ -129,6 +129,7 @@ https://solved.ac/profile/neneg273
 15654: N과 M (5) BT https://www.acmicpc.net/problem/15654  
 15657: N과 M (8) BT https://www.acmicpc.net/problem/15657  
 15663: N과 M (9) BT https://www.acmicpc.net/problem/15663  
+15666: N과 M (12) BT https://www.acmicpc.net/problem/15666  
 15686: 치킨 배달 BT https://www.acmicpc.net/problem/15686  
 15810: 풍선 공장 BS https://www.acmicpc.net/problem/15810  
 15829: Hashing https://www.acmicpc.net/problem/15829  
