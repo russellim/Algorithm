3042ea3faae03abe847497973488583c10fad161
russellim <neneg273@gmail.com>
2021-03-03 18:33:59 +0900

    11725_트리의 부모 찾기.cpp solved!

--- BeakJoon/11725_트리의 부모 찾기.cpp
+++ BeakJoon/11725_트리의 부모 찾기.cpp
@@ -1,25 +1,60 @@
 ﻿// 21.03.01. 월
 // 11725: 트리의 부모 찾기 https://www.acmicpc.net/problem/11725
-// 트리, BFS, DFS.
-
-// 83% 시간초과.
-
+// DFS.
 #include <iostream>
 #include <deque>
 #include <vector>
 
 using namespace std;
 
+// dfs. 48ms solved.
 namespace BOJ_11725
 {
 	vector<int> Tree;
-	deque<pair<int, int> > ReadyNodes;
+	vector<vector<int> > connNode;
+	vector<bool> visit;
 
-	void InsertTree(int a, int b)
+	void dfs(int parent)
 	{
-		if (Tree[a]) Tree[b] = a;
-		else Tree[a] = b;
+		for (int i = 0; i < connNode[parent].size(); ++i)
+		{
+			int child = connNode[parent][i];
+			if (visit[child]) continue;
+			visit[child] = true;
+			Tree[child] = parent;
+			dfs(child);
+		}
 	}
+
+	void Solution()
+	{
+		int n, a, b;
+		cin >> n;
+		Tree.assign(n + 1, 0);
+		connNode.assign(n + 1, vector<int>());
+		visit.assign(n + 1, false);
+		visit[1] = true;
+
+		for (int i = 0; i < n - 1; ++i)
+		{
+			cin >> a >> b;
+			connNode[a].push_back(b);
+			connNode[b].push_back(a);
+		}
+		dfs(1);
+
+		for (int i = 2; i <= n; ++i)
+		{
+			cout << Tree[i] << "\n";
+		}
+	}
+}
+
+// deque 사용 83% 시간초과.
+namespace BOJ_11725_legacy
+{
+	vector<int> Tree;
+	deque<pair<int, int> > ReadyNodes;
 
 	void CheckReadyNodes()
 	{
@@ -29,16 +64,12 @@ namespace BOJ_11725
 			tempdq.swap(ReadyNodes);
 			while (!tempdq.empty())
 			{
-				int a = tempdq.front().first;
-				int b = tempdq.front().second;
-				tempdq.pop_front();
-				if (Tree[a] == 0 && Tree[b] == 0)
-				{
-					// 다음에는 거꾸로 검사.
-					ReadyNodes.push_front({ a, b });
-					continue;
-				}
-				InsertTree(a, b);
+				int a = tempdq.back().first;
+				int b = tempdq.back().second;
+				tempdq.pop_back();
+				if (Tree[a]) Tree[b] = a;
+				else if (Tree[b]) Tree[a] = b;
+				else ReadyNodes.push_back({ a, b });
 			}
 		}
 	}
@@ -52,12 +83,9 @@ namespace BOJ_11725
 		for (int i = 0; i < n - 1; ++i)
 		{
 			cin >> a >> b;
-			if (Tree[a] == 0 && Tree[b] == 0)
-			{
-				ReadyNodes.push_back({ a, b });
-				continue;
-			}
-			InsertTree(a, b);
+			if (Tree[a]) Tree[b] = a;
+			else if (Tree[b]) Tree[a] = b;
+			else ReadyNodes.push_back({ a, b });
 		}
 
 		CheckReadyNodes();
