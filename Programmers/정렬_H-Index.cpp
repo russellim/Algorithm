3a0a042f9ea1b5152b704b4389bbed38714903c8
russellim <neneg273@gmail.com>
2021-03-08 19:48:30 +0900

    정렬_H-Index.cpp

---
+++ Programmers/정렬_H-Index.cpp
@@ -0,0 +1,49 @@
+﻿// 21.03.08. 월
+// 정렬_H-Index https://programmers.co.kr/learn/courses/30/lessons/42747
+// 정렬. 근데 문제이해가 더 어려웠음.
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(pair<int, int>& a, pair<int, int>& b)
+{
+	return a.first > b.first;
+}
+
+int solution(vector<int> citations) {
+	int answer = 0, count = 0;
+	int size = citations.size();
+
+	vector<pair<int, int> > v;
+	for (int i = 0; i < size; ++i)
+	{
+		v.push_back({ citations[i], i + 1 });
+	}
+	sort(v.begin(), v.end(), cmp);
+	for (int i = 0; i < size; ++i)
+	{
+		if (i >= v[i].first) return i;
+	}
+	if (answer == 0) answer = size;
+	return answer;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cout << solution({ 3, 0, 6, 1, 5 }) << "\n";//3
+	cout << solution({ 1, 2, 3, 4, 5 }) << "\n";//3
+	cout << solution({ 0, 0, 0, 0, 5 }) << "\n";//1
+	cout << solution({ 10,8,5,4,3 }) << "\n";	//4
+	cout << solution({ 25,8,5,3,3 }) << "\n";	//3
+	cout << solution({ 1, 2, 3, 3, 3, 3, 4, 4, 5, 6, 7, 7, 8, 8, 9, 9, 10, 10, 10 }) << "\n";	//7
+	cout << solution({ 2, 2 }) << "\n";	//2
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -162,6 +162,7 @@ kakao 2020인턴_키패드 누르기 https://programmers.co.kr/learn/courses/30/
 스택/큐_프린터 https://programmers.co.kr/learn/courses/30/lessons/42587  
 힙_더 맵게 https://programmers.co.kr/learn/courses/30/lessons/42626  
 정렬_가장 큰 수 https://programmers.co.kr/learn/courses/30/lessons/42746  
+정렬_H-Index https://programmers.co.kr/learn/courses/30/lessons/42747  
 kakao 2017_카카오프렌즈 컬러링북 https://programmers.co.kr/learn/courses/30/lessons/1829  
 kakao 2018_뉴스 클러스터링  https://programmers.co.kr/learn/courses/30/lessons/17677  
 kakao 2018_프렌즈4블록 https://programmers.co.kr/learn/courses/30/lessons/17679  
