c26600badbb88ed5f3991b74b95074a152e8d086
russellim <neneg273@gmail.com>
2021-03-09 20:17:39 +0900

    탐욕법_섬 연결하기.cpp

---
+++ Programmers/탐욕법_섬 연결하기.cpp
@@ -0,0 +1,65 @@
+﻿// 21.03.09. 화
+// 탐욕법_섬 연결하기 https://programmers.co.kr/learn/courses/30/lessons/42861
+// 프림 알고리즘 사용.
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct Connect
+{
+	int dest, cost;
+};
+
+int answer = 0, connCount = 0;
+vector<vector<Connect> > conn;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq; // {cost, from}.
+vector<bool> visit;
+
+void FindBridge(int start)
+{
+	visit[start] = true;
+	connCount++;
+
+	for (int i = 0; i < conn[start].size(); ++i)
+	{
+		if (visit[conn[start][i].dest]) continue;
+		pq.push({ conn[start][i].cost, conn[start][i].dest });
+	}
+}
+
+int solution(int n, vector<vector<int>> costs) {
+	conn.assign(n, vector<Connect>());
+	visit.assign(n, false);
+	for (int i = 0; i < costs.size(); ++i)
+	{
+		conn[costs[i][0]].push_back({ costs[i][1], costs[i][2] });
+		conn[costs[i][1]].push_back({ costs[i][0], costs[i][2] });
+	}
+
+	FindBridge(0);
+
+	while (connCount != n)
+	{
+		auto next = pq.top();
+		pq.pop();
+		if (visit[next.second]) continue;
+		answer += next.first;
+		FindBridge(next.second);
+	}
+
+	return answer;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cout << solution(4, { {0,1,1},{0,2,2},{1,2,5},{1,3,1},{2,3,8} });
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -180,6 +180,7 @@ kakap 2020인턴_수식 최대화 https://programmers.co.kr/learn/courses/30/les
 #### Level 3
 해시_베스트앨범 https://programmers.co.kr/learn/courses/30/lessons/42579  
 힙_디스크 컨트롤러 https://programmers.co.kr/learn/courses/30/lessons/42627  
+탐욕법_섬 연결하기 https://programmers.co.kr/learn/courses/30/lessons/42861  
 DFS/BFS_네트워크 https://programmers.co.kr/learn/courses/30/lessons/43162  
 그래프_가장 먼 노드 BFS https://programmers.co.kr/learn/courses/30/lessons/49189  
 kakao 2018_추석 트래픽 https://programmers.co.kr/learn/courses/30/lessons/17676  
