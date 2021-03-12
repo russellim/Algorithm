06183bd5988dd3da2a74c493b558965931d204a3
russellim <neneg273@gmail.com>
2021-03-12 11:23:50 +0900

    DFS,BFS_단어 변환.cpp 잘못올림수정

--- Programmers/DFS,BFS_단어 변환.cpp
+++ Programmers/DFS,BFS_단어 변환.cpp
@@ -1,98 +1,48 @@
-﻿// 21.03.10. 수
-// 탐욕법_단속카메라 https://programmers.co.kr/learn/courses/30/lessons/42884
-
-#include <iostream>
+﻿// 21.03.11. 목
+// DFS/BFS_단어 변환 https://programmers.co.kr/learn/courses/30/lessons/43163
+// BFS.
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
-// 도로를 빨리 나가는 순으로 나가는 시점에 설치.
-vector<bool> visit;
-
-bool cmp(vector<int>& a, vector<int>& b)
+int NumOfDifferent(string s1, string s2)
 {
-	return a[1] < b[1];
-}
-
-void CheckOverlap(vector<vector<int>>& routes, int end)
-{
-	for (int i = 0; i < routes.size(); ++i)
+	int different = 0;
+	for (int i = 0; i < s1.size(); ++i)
 	{
-		if (visit[i]) continue;
-		if (end >= routes[i][0] && end <= routes[i][1])
-		{
-			visit[i] = true;
-		}
+		if (s1[i] != s2[i]) different++;
 	}
+	return different;
 }
 
-int solution(vector<vector<int>> routes) {
+int solution(string begin, string target, vector<string> words) {
 	int answer = 0;
-	visit.assign(routes.size(), false);
-	sort(routes.begin(), routes.end(), cmp);
-
-	for (int i = 0; i < routes.size(); ++i)
+	if (find(words.begin(), words.end(), target) == words.end())
 	{
-		if (visit[i]) continue;
-		visit[i] = true;
-		CheckOverlap(routes, routes[i][1]);
-		answer++;
+		return 0;
+	}
+
+	vector<bool> visit(words.size(), false);
+	queue<pair<string, int> > q;
+	q.push({ begin, 0 });
+
+	while (!q.empty())
+	{
+		string nowStr = q.front().first;
+		int nowCount = q.front().second;
+		q.pop();
+		for (int i = 0; i < words.size(); ++i)
+		{
+			if (visit[i]) continue;
+			if (NumOfDifferent(nowStr, words[i]) != 1) continue;
+			if (words[i] == target) return nowCount + 1;
+			visit[i] = true;
+			q.push({ words[i], nowCount + 1 });
+		}
 	}
 
 	return answer;
 }
-
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-
-	cout << solution({ {-20, 15}, {-14, -5}, {-18, -13}, {-5, -3} }) << "\n";
-	cout << solution({ {1,3}, {5,8} }) << "\n";
-	cout << solution({ {0, 4}, {0, 3}, {0, 2}, {0, 1} }) << "\n";
-	cout << solution({ {0,2},{2,3},{3,4},{4,6} }) << "\n";
-
-	return 0;
-}
-
-/*
- 정확성은 다 맞는데 효율성에서 그냥 틀린 코드.
- 이동한 거리가 짧은 순으로 앞뒤에 먼저 설치.
- */
- //vector<bool> visit;
- //
- //bool cmp(vector<int>& a, vector<int>& b)
- //{
- //	return ((a[1] - a[0]) < (b[1] - b[0]));
- //}
- //
- //void CheckOverlap(vector<vector<int>>& routes, int start, int end)
- //{
- //	for (int i = 0; i < visit.size(); ++i)
- //	{
- //		if (visit[i]) continue;
- //		if ((start >= routes[i][0] && start <= routes[i][1]) ||
- //			(end >= routes[i][0] && end <= routes[i][1]))
- //		{
- //			visit[i] = true;
- //		}
- //	}
- //}
- //
- //int solution(vector<vector<int>> routes) {
- //	int answer = 0;
- //	sort(routes.begin(), routes.end(), cmp);
- //	visit.assign(routes.size(), false);
- //
- //	for (int i = 0; i < routes.size(); ++i)
- //	{
- //		if (visit[i]) continue;
- //		visit[i] = true;
- //		CheckOverlap(routes, routes[i][0], routes[i][1]);
- //		answer++;
- //	}
- //
- //	return answer;
- //}
