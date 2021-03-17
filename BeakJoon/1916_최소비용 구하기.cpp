01bf5a8062e012653934863ba6d88b7050e4b25a
russellim <neneg273@gmail.com>
2021-03-17 11:30:11 +0900

    1916_최소비용 구하기.cpp

---
+++ BeakJoon/1916_최소비용 구하기.cpp
@@ -0,0 +1,69 @@
+﻿// 21.03.17. 수
+// 1916: 최소비용 구하기 https://www.acmicpc.net/problem/1916
+// 다익스트라.
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+namespace BOJ_1916
+{
+	struct Dest
+	{
+		int cost, from;
+	};
+
+	// 다익스트라.
+	int GetMinCost(int& n, vector<vector<Dest> >& dest, int start, int end)
+	{
+		priority_queue<pair<int, int>> pq;
+		vector<int> minCost(n + 1, INT_MAX);
+		minCost[start] = 0;
+		pq.push({ 0, start });
+
+		while (!pq.empty())
+		{
+			int nowCost = pq.top().first;
+			int nowPos = pq.top().second;
+			pq.pop();
+			for (int i = 0; i < dest[nowPos].size(); ++i)
+			{
+				int nextCost = dest[nowPos][i].cost;
+				int nextPos = dest[nowPos][i].from;
+				if (nowCost + nextCost >= minCost[nextPos]) continue;
+				minCost[nextPos] = nowCost + nextCost;
+				pq.push({ minCost[nextPos] , nextPos });
+			}
+		}
+
+		return minCost[end];
+	}
+
+	void Solution()
+	{
+		int n, m;
+		cin >> n >> m;
+		vector<vector<Dest> > dest(n + 1, vector<Dest>());
+		for (int i = 0; i < m; ++i)
+		{
+			int to, from, cost;
+			cin >> to >> from >> cost;
+			dest[to].push_back({ cost, from });
+		}
+		int start, end;
+		cin >> start >> end;
+		cout << GetMinCost(n, dest, start, end);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	BOJ_1916::Solution();
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -37,6 +37,7 @@ https://solved.ac/profile/neneg273
 1786: 찾기 KMP https://www.acmicpc.net/problem/1786  
 1874: 스택 수열 Stack https://www.acmicpc.net/problem/1874  
 1912: 연속합 DP https://www.acmicpc.net/problem/1912  
+1916: 최소비용 구하기 Dijkstra https://www.acmicpc.net/problem/1916  
 1920: 수 찾기 BinarySearch https://www.acmicpc.net/problem/1920  
 1927: 최소 힙 https://www.acmicpc.net/problem/1927  
 1931: 회의실배정 Sort https://www.acmicpc.net/problem/1931  
