277cebb14ba951b337e09df4dccd844fccebc134
russellim <neneg273@gmail.com>
2021-05-09 21:24:15 +0900

    2019겨울인턴_호텔 방 배정.cpp

--- Programmers/2019겨울인턴_호텔 방 배정.cpp
+++ Programmers/2019겨울인턴_호텔 방 배정.cpp
@@ -1,35 +1,39 @@
 ﻿// 21.05.07. 금
 // kakao 2019겨울인턴_호텔 방 배정 https://programmers.co.kr/learn/courses/30/lessons/64063
-
-// failed 효율성 탈락 -> 탐색 최소로.
 // 데이터가 많을때는 map보다 umap의 find가 더 빠르다.
-// 메모리 초과는 해결!.
 #include <string>
 #include <vector>
 #include <unordered_map>
 
 using namespace std;
 
+unordered_map<long long, long long> room;   // 방번호, 다음 방번호.
+
+long long Find(long long n)
+{
+    if (room[n] == 0) return n;
+    room[n] = Find(room[n]);
+    return room[n];
+}
+
 vector<long long> solution(long long k, vector<long long> room_number) {
-    unordered_map<long long, bool> room;
-    long long pointer = 1;
     int peopleSize = room_number.size();
     vector<long long> answer(peopleSize);
 
     for (int i = 0; i < peopleSize; ++i)
     {
         long long wantRoom = room_number[i];
-        if (room[wantRoom])
+        if (room[wantRoom] == 0)
         {
-            if (wantRoom < pointer)
-            {
-                wantRoom = pointer;
-            }
-            while (room[wantRoom]) wantRoom++;
+            answer[i] = wantRoom;
+            room[wantRoom] = Find(wantRoom + 1);
         }
-        room[wantRoom] = true;
-        answer[i] = wantRoom;
-        while (room[pointer]) pointer++;
+        else
+        {
+            long long nextRoom = Find(wantRoom);
+            answer[i] = nextRoom;
+            room[nextRoom] = Find(nextRoom + 1);
+        }
     }
     return answer;
 }
