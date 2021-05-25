591f635622af790271a4686ceec47aab97fc66a1
russellim <neneg273@gmail.com>
2021-05-25 17:44:42 +0900

    1915_가장 큰 정사각형.cpp 메모리줄임

--- BeakJoon/1915_가장 큰 정사각형.cpp
+++ BeakJoon/1915_가장 큰 정사각형.cpp
@@ -13,18 +13,14 @@ namespace BOJ_2225
 	int GetDP(vector<vector<int> >& pann, int n, int m)
 	{
 		int res = pann[0][0];
-		vector<vector<int> >dp(n, vector<int>(m, 0));
-
-		for (int i = 0; i < n; ++i) dp[i][0] = pann[i][0];
-		for (int j = 0; j < m; ++j) dp[0][j] = pann[0][j];
 
 		for (int i = 1; i < n; ++i)
 		{
 			for (int j = 1; j < m; ++j)
 			{
 				if (pann[i][j] == 0) continue;
-				dp[i][j] = min({ dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] }) + 1;
-				res = max(res, dp[i][j]);
+				pann[i][j] = min({ pann[i - 1][j], pann[i][j - 1], pann[i - 1][j - 1] }) + 1;
+				res = max(res, pann[i][j]);
 			}
 		}
 
