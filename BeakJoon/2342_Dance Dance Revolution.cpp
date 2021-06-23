d65f4e8655cc01940369289c1d8032b1670ca6c7
russellim <neneg273@gmail.com>
2021-06-23 23:34:32 +0900

    2342_Dance Dance Revolution.cpp failed 시간초과

---
+++ BeakJoon/2342_Dance Dance Revolution.cpp
@@ -0,0 +1,59 @@
+﻿// 21.06.23. 수
+// 2342: Dance Dance Revolution https://www.acmicpc.net/problem/2342
+// DP, DFS.
+
+// failed. 시간초과.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+namespace BOJ_2342
+{
+	constexpr int MAX = 100000;
+	constexpr int DIR = 5;
+	vector<int> cmd;
+	vector<vector<vector<int> > > dp(MAX, vector<vector<int> >(DIR, vector<int>(DIR, -1)));
+
+	int CheckScore(int from, int to)
+	{
+		if (from == to) return 1; // 같은곳.
+		if (from == 0) return 2;  // 0에서 이동..
+		if (abs(from - to) == 2) return 4; // 반대편 이동.
+		return 3; // 인접한곳 이동.
+	}
+
+	int dfs(int foot1, int foot2, int index)
+	{
+		if (index == cmd.size()) return 0;
+
+		int& now = dp[index][foot1][foot2];
+		if (now != -1) return now;
+
+		return now = min(dfs(cmd[index], foot2, index + 1) + CheckScore(foot1, cmd[index]),
+			dfs(foot1, cmd[index], index + 1) + CheckScore(foot2, cmd[index]));
+	}
+
+	void Solution()
+	{
+		int point;
+		while (1)
+		{
+			cin >> point;
+			if (point == 0) break;
+			cmd.reserve(cmd.size() + 1);
+			cmd.emplace_back(point);
+		}
+
+		cout << dfs(0, 0, 0);
+	}
+}
+
+int main()
+{
+	BOJ_2342::Solution();
+
+	return 0;
+}
