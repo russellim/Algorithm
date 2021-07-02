e35d17026ee125c0a1aa54122235bdeea6d06320
russellim <neneg273@gmail.com>
2021-07-02 23:34:10 +0900

    1725_히스토그램.cpp 푸는중

---
+++ BeakJoon/1725_히스토그램.cpp
@@ -0,0 +1,30 @@
+﻿// 21.07.02. 금
+// 1725: 히스토그램 https://www.acmicpc.net/problem/1725
+
+// 푸는중.. 플레5!
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+namespace BOJ_1725
+{
+	void Solution()
+	{
+		int n;
+		cin >> n;
+		vector<int> arr(n);
+		for (int& x : arr) cin >> x;
+
+
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	BOJ_1725::Solution();
+	return 0;
+}
