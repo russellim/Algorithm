66edb657181a481ed70a049374bd28e4bdc279c7
russellim <neneg273@gmail.com>
2021-07-08 22:40:52 +0900

    폰켓몬.js update

--- Programmers/js/폰켓몬.js
+++ Programmers/js/폰켓몬.js
@@ -1,16 +1,20 @@
 /*
 21_07_07 create.
-21_07_08 update Set, forEach 사용.
+21_07_08 update: Set, forEach 사용.
+21_07_08 update: forEach 주석처리하고 바로 넣어줌.
 */
 
 function solution(nums) {
     const numsSize = nums.length / 2;
-    let set = new Set();
+
+//     let set = new Set();
+//     // 반복문!
+//     nums.forEach(function(item) {
+//         set.add(item);
+//     });
     
-    // 반복문!
-    nums.forEach(function(item) {
-        set.add(item);
-    });
+    // 반복문 사용 안하고 바로 넣기.
+    let set = new Set(nums);
 
     var setSize = set.size;
     return ((setSize >= numsSize) ? numsSize : setSize);
