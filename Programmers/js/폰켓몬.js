bf83567a7e0f5f355bf96183a41bd8a3cf03e4ec
russellim <neneg273@gmail.com>
2021-07-11 22:49:19 +0900

    log

--- Programmers/js/폰켓몬.js
+++ Programmers/js/폰켓몬.js
@@ -3,6 +3,7 @@ https://programmers.co.kr/learn/courses/30/lessons/1845
 21_07_07 create.
 21_07_08 update: Set, forEach 사용.
 21_07_08 update: forEach 주석처리하고 바로 넣어줌.
+21_07_08 solved!
 */
 
 function solution(nums) {
