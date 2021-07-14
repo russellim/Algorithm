7478855b37d53df014146943dc97b7d93ef73b10
russellim <neneg273@gmail.com>
2021-07-14 23:42:31 +0900

    메뉴 리뉴얼.js

---
+++ Programmers/js/메뉴 리뉴얼.js
@@ -0,0 +1,15 @@
+/*
+https://programmers.co.kr/learn/courses/30/lessons/72411
+21_07_14 create.
+
+*/
+
+
+function solution(orders, course) {
+    var answer = [];
+    return answer;
+}
+
+console.log(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]));
+console.log(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]));
+console.log(solution(["XYZ", "XWY", "WXA"], [2,3,4]));
