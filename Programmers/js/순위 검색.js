d727410ef242bf27d6fff79796675dbfb52a1981
russellim <neneg273@gmail.com>
2021-07-31 23:19:26 +0900

    순위 검색.js 시간초과

--- Programmers/js/순위 검색.js
+++ Programmers/js/순위 검색.js
@@ -3,8 +3,7 @@ https://programmers.co.kr/learn/courses/30/lessons/72412
 21_07_18 create.
 21_07_19 ~ 21_07_24 update: 문자열 자르기 추가. str.split(' ') -> array 반환, foreach, 쿼리 검색 매치,  Number() 사용, 제출했는데 시간 초과
 21_07_25 update: Map 써서 새로운 방법으로 해보자
-21_07_26 update: - 넣는 재귀함수 추가
-21_07_27 update: 점수 추가 Map 대신 2차원 배열을 써야할 것 같음
+21_07_31 update: 시간초과
 */
 
 var infoMap = [];
@@ -56,7 +55,6 @@ function solution(info, query){
     query.forEach(value =>{
          searchArr.push(value.split(' and '));
     });
-    count = 0;
     for(let i=0; i<searchArr.length; ++i){
         let temp = searchArr[i][3];
         let splitTemp = temp.split(' ');
@@ -65,11 +63,14 @@ function solution(info, query){
         
         let searchStr = arrayToString(searchArr[i]);
         let searchScore = splitTemp[1];
+        count = 0;
         
         for(let infoIndex = 0; infoIndex<infoMap.length; ++infoIndex){
-            if(infoMap[infoIndex][0] < searchScore) break;
-            console.log(infoMap[infoIndex][1] + " " + searchStr);
-            if(infoMap[infoIndex][1] == searchStr) ++count;
+            if(Number(infoMap[infoIndex][0]) < Number(searchScore)) break;
+            if(infoMap[infoIndex][1] == searchStr) 
+            {
+                ++count;
+            }
         }
         answer.push(count);
     }
