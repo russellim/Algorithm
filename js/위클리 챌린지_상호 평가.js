f831ad62ad7d034000aba3516745362e15add042
russellim <38000693+russellim@users.noreply.github.com>
2021-08-15 22:47:04 +0900

    Update and rename 상호 평가.js to 위클리 챌린지_상호 평가.js
    solved

--- js/위클리 챌린지_상호 평가.js
+++ js/위클리 챌린지_상호 평가.js
@@ -1,3 +1,4 @@
+// 점수 -> 학점.
 function getGrade(score)
 {
     if(score >= 90) return 'A';
@@ -11,6 +12,7 @@ function solution(scores) {
     var answer = '';
     let numOfStudent = scores.length;
     
+    // 배열 행, 열 반대로 새 배열에 넣기.
     let myScore = [];
     
     for(let j=0; j<numOfStudent; ++j){
@@ -21,21 +23,28 @@ function solution(scores) {
         myScore.push(tempScore);
     }
     
+    // 최고점, 최저점 찾기.
     for(let i=0; i<numOfStudent; ++i){
         let maxScore = Math.max(...myScore[i]);
         let minScore = Math.min(...myScore[i]);
+        let exceptMe = 0; // 내가 최고점 or 최저점일때 제외됨.
         
+        // 내가 유일한 최고점 or 최저점인지 확인.
         if(myScore[i][i] == maxScore
           && myScore[i].reduce((cnt, value) => cnt + (maxScore == value), 0) == 1) {
             myScore[i][i] = 0;
+            exceptMe++;
         }
         else if(myScore[i][i] == minScore &&
           myScore[i].reduce((cnt, value) => cnt + (minScore == value), 0) == 1) {
              myScore[i][i] = 0;
+            exceptMe++;
         }
+        
+        // 평균, 학점 구하기.
+        let sum = myScore[i].reduce((sum, value) => sum + value, 0);
+        answer += getGrade(sum / (numOfStudent - exceptMe));
     }
-    
-    answer += getGrade(myScore[i].reduce((sum, value) => cnt + value) / numOfStudent);
         
     return answer;
 }
