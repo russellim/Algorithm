ad50db78ee3f150cbc99fd479012fc9e5a1f79cb
russellim <38000693+russellim@users.noreply.github.com>
2021-08-19 23:52:56 +0900

    Update 위클리 챌린지_퍼즐 조각 채우기.js

--- js/위클리 챌린지_퍼즐 조각 채우기.js
+++ js/위클리 챌린지_퍼즐 조각 채우기.js
@@ -1,6 +1,7 @@
 // table에서 도형들 분리한 뒤, 배열에 넣어줌. (좌표), 블록 갯수도 저장.
 // game_board 탐색하면서 빈칸 만나면 그 빈칸 갯수만큼의 도형을 대조함.
 // 맞는 도형이 있으면 도형의 블록 갯수만큼 반환하고 해당 도형 삭제.
+//figure is [x,y]
 
 // BFS이용해서 빈칸 개수 세고, 빈칸에 맞춰보기
 function compareEmpty(game_board, figures){
@@ -26,5 +27,7 @@ function makeFigure(figures){
 function solution(game_board, table) {
     var answer = -1;
     var figures = [];
+    
+    
     return answer;
 }
